46c09ce2b60184e258db34ea644bf85574e301b9
Rupesh Jeyaram <rjeyaram@caltech.edu>
2019-04-13 16:32:33 -0700

    Store MBR of data points

--- Python_Files/load_TROPOMI.py
+++ Python_Files/load_TROPOMI.py
@@ -13,6 +13,8 @@
 
 SOURCE = '../TROPOMI_Data/'
 
+INSERT_PREFIX = 'INSERT INTO tropomi VALUES '
+
 import os						# For searching through files
 import netCDF4 as nc 			# For parsing data format
 import mysql.connector as dbapi # For connecting to database
@@ -45,24 +47,33 @@ for file in sorted(os.listdir(SOURCE)):
 		# It takes orders of magnitude longer to use the 
 		# native Dataset format
 
+		# print(keys)
+		# print(np.array(nc_file.variables['bnds']))
+		# print(np.array(nc_file.variables['lat_bnds']))
+		# print(np.array(nc_file.variables['lon_bnds']))
+		# break
+
 		datetimes = np.array(nc_file.variables['TIME'])
 		sifs = np.array(nc_file.variables['sif'])
 		lats = np.array(nc_file.variables['lat'])
 		lons = np.array(nc_file.variables['lon'])
+		lat_bnds = np.array(nc_file.variables['lat_bnds'])
+		lon_bnds = np.array(nc_file.variables['lon_bnds'])
 
 		num_rows = len(datetimes)
 
 		# Enter each record into the database
 
 		# Use this string to construct a batch insert SQL statement
-		cmd = 'INSERT INTO tropomi VALUES '
+		cmd = INSERT_PREFIX
 
 		t1 = time.time()
 
 		for i in range(num_rows):
 
 			# Insert rows in chunks of 100,000 records
-			if ((i+1) % 100000 == 0):
+			# 100000
+			if ((i+1) % 60 == 0):
 				print(str(i/num_rows * 100) + '% completed')
 
 				cmd = cmd[:-2]
@@ -72,20 +83,34 @@ for file in sorted(os.listdir(SOURCE)):
 				cursor.execute(cmd)
 				conn.commit()
 				print('time to insert: ' + str(time.time() - t2))
-				cmd = 'INSERT INTO tropomi VALUES '
+				cmd = INSERT_PREFIX
 
 				t1 = time.time()
+				break
 
 			# Convert from utc to datetime
 			date = datetime.utcfromtimestamp(datetimes[i]).strftime\
 				('%Y-%m-%d %H:%M:%S')
 
+			# Extract minimum bounding rectangle coordinates
+			# reminder: lon corresponds to x, lat corresponds to y
+
+			# top left corner
+			mbr_tlc_lat = max(lat_bnds[:,i])	
+			mbr_tlc_lon = min(lon_bnds[:,i])
+
+			# bottom right corner
+			mbr_brc_lat = min(lat_bnds[:,i])	
+			mbr_brc_lon = max(lon_bnds[:,i])
+
 			# Append to SQL statement
-			cmd += '(NULL, \' %s \' , %s, %s, %s), ' % \
-					(date, sifs[i], lats[i], lons[i])
+			cmd += '(NULL, \' %s \' , %s, %s, %s, %s, %s, %s, %s), ' % \
+					(date, sifs[i], lats[i], lons[i], \
+					 mbr_tlc_lat, mbr_tlc_lon, mbr_brc_lat, mbr_brc_lon)
 		
-		cursor.execute(cmd[:-2])
-		conn.commit()
+		if (cmd != INSERT_PREFIX):
+			cursor.execute(cmd[:-2])
+			conn.commit()
 
 		break
 
